8410172cc5fc2bb26f58b8775729d986f5004607
Sagar Patel <sagar.patel@byteparity.com>
2017-02-17 14:16:50 +0530

    cors file include

---
+++ server/config/cors.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    /*
+     |--------------------------------------------------------------------------
+     | Laravel CORS
+     |--------------------------------------------------------------------------
+     |
+     | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+     | to accept any value.
+     |
+     */
+    'supportsCredentials' => false,
+    'allowedOrigins' => ['https://byteparity.github.io/Movies-Database-Laravel-AngualrJS-2/'],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'],
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+    'hosts' => [],
+];
+
