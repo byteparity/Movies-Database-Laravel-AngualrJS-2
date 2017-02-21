fdd74ff438342f0a93eeeaa2b0002070e36f8009
Sagar Patel <sagar.patel@byteparity.com>
2017-02-21 19:07:39 +0530

    changes in css and js include method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { MoviesListComponent } from './movies-list/movies-list.component';
 @Component({
   selector: 'app-root',
   templateUrl:'./app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './polyfills.ts';
-import '@angular/material/core/theming/prebuilt/indigo-pink.css'
+import '@angular/material/core/theming/prebuilt/indigo-pink.css';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
